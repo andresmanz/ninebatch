13311a8433ab76e30ece6cda3bfd93abeb0a269a
Andres Manz <manz.andres@gmail.com>
2014-11-27 19:42:15 +0100

    Add publish script for travis

---
+++ util/publish-snapshot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "andresmanz/ninepatch-convert" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
+        echo 'Travis can only publish snapshots.'
+        return 0
+    fi
+
+    echo -e "Starting publish to Sonatype...\n"
+
+    ./gradlew uploadArchives -PossrhUsername="${SONATYPE_USERNAME}" -PossrhPassword="${SONATYPE_PASSWORD}"
+    RETVAL=$?
+
+    if [ $RETVAL -eq 0 ]; then
+        echo 'Completed publish!'
+    else
+        echo 'Publish failed.'
+        return 1
+    fi
+fi
